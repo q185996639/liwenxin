ef6cf41d3e39d3c2e1f81a1a2a67737b426b0197
q185996639 <185996639@qq.com>
2017-01-04 19:35:24 +0800

    web2.0

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 if (location.hash) {
 	var hash = location.hash;
 	$(hash).show().siblings().hide();
-	console.log(1)
+	console.log(1)//我是2.0
 }
 window.onhashchange = function(){
 	var hash = location.hash;
